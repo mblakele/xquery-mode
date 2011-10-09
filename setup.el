893b012fc2f28b764b250314a4bc3370c5db38de
Michael Blakeley <mike@blakeley.com>
2011-10-09 15:01:13 -0700

    remove external write hook

--- setup.el
+++ setup.el
@@ -3,7 +3,3 @@
 (autoload 'xquery-mode "xquery-mode" "XQuery mode" t )
 (setq auto-mode-alist
       (append '(("\\.xqy$" . xquery-mode)) auto-mode-alist))
-(add-hook 'xquery-mode-hook
-  '(lambda ()
-     (make-local-variable 'write-contents-hooks)
-     (add-hook 'write-contents-hooks 'buffer-replace-all-tabs)))
